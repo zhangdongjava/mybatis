6c0cba734adb9bb1d362e986341377a615bd1f2f
zhangdongjava <787395919@qq.com>
2018-02-07 17:29:58 +0800

    Initial commit

---
+++ mybatis-3-mybatis-3.4.5/src/test/java/Demo.java
@@ -0,0 +1,26 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Administrator on 2018/2/7.
+ */
+public class Demo {
+
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            Object blog = session.selectList("com.zzz.demo.mapper.DaiLiMapper.getCount");
+            System.out.println(blog);
+        } finally {
+            session.close();
+        }
+    }
+}
---
+++ mybatis-3-mybatis-3.4.5/src/test/java/com/zzz/demo/mapper/DaiLiMapper.java
@@ -0,0 +1,7 @@
+package com.zzz.demo.mapper;
+
+/**
+ * Created by Administrator on 2018/2/7.
+ */
+public class DaiLiMapper {
+}
